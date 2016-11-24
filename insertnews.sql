da73c31ced035dd447eae1826a2a41050e411531
Ptara108 <andreiabarros_95@hotmail.com>
2016-11-24 20:01:30 +0000

    Add files via upload

--- insertnews.sql
+++ insertnews.sql
@@ -1,5 +1,6 @@
 INSERT INTO news(news) 
-	select 'A tua prima'
+	select 'A tua prima 4'
     from news
-	WHERE not exists(select id_N from news where news.news = 'A tua prima');
-    
+    WHERE not exists(select id_N from news where news.news = 'A tua prima 4')
+    LIMIT 1;
+	
